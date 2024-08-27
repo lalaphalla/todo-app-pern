d5052ad51d8b11b9ec66e7046614e596fbbb5c92
Pav Munyphalla <lalaphalla@gmail.com>
2024-08-27 21:41:54 +0700

    update URL

--- client/src/apis/taskApi.js
+++ client/src/apis/taskApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const tasksApi = createApi({
   reducerPath: "tasks",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:3000/api/v1",
+    baseUrl: "api/v1",
   }),
   endpoints(builder) {
     return {
